42d3b9c7e00ffc73a963128fc38b587019bec091
ilyesbrh <brhilyes@gmail.com>
2023-02-12 14:27:06 +0100

    commit

--- api.py
+++ api.py
@@ -3,11 +3,14 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+model = Word2Vec.load("model_2022.bin")
+
 @app.route('/')
 def word2vec():
-    model = Word2Vec.load("model/trained_model.bin")
-    w = request.args['word']
-    return jsonify(model.most_similar(positive=[w], topn=10))
+    w = request.args.get('word')
+    
+    # return jsonify(model.wv.index_to_key)
+    return jsonify(model.wv.most_similar(positive=[w], topn=10))
 
 if __name__ == '__main__':
     app.run(debug=True)
